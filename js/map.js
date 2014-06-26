1c4af8897f742f1c7abc6e774bfa75eafc0e49a8
Eric Brelsford <ebrelsford@gmail.com>
2014-06-26 16:44:17 -0400

    Remove cartodb.js and use GeoJSON

--- js/map.js
+++ js/map.js
@@ -1,7 +1,5 @@
 var map;
 
-var defaultCartoCSS = '#food_worker_orgs{ marker-fill: #0F0; }';
-
 function initializeMap(id) {
     map = L.map(id, {
         center: [39.095963, -97.470703],
@@ -19,38 +17,15 @@ function initializeMap(id) {
         maxZoom: 18
     }).addTo(map);
 
-    cartodb.createLayer(map, {
-        cartodb_logo: false,
-        user_name: 'fcwa',
-        type: 'cartodb',
-        sublayers: [{
-            cartocss: defaultCartoCSS,
-            interactivity: 'address, city, name, organization_type, state, food_sector_s',
-            sql: 'SELECT * FROM food_worker_orgs'
-        }]
-    })
-    .addTo(map)
-    .done(function (layer) {
-        lotsLayer = layer.getSubLayer(0);
-        lotsLayer.setInteraction(true);
-        layer.on('featureClick', function (e, latlng, pos, data, sublayerIndex) {
-            map.fire('featureclick', data);
-        });
-
-        layer.on('featureOver', function (e, latlng, pos, data) {
-            // Update mouse cursor when over a feature
-            $map.css('cursor', 'pointer');
-            data.latlng = latlng;
-            map.fire('featureover', data);
-        });
-        layer.on('featureOut', function (e, latlng, pos, data) {
-            // Reset mouse cursor when no longer over a feature
-            var grabStyle = 'cursor: grab; cursor: -moz-grab; cursor: -webkit-grab;';
-            $map.attr('style',  grabStyle);
-            map.fire('featureout', data);
-        });
-
-        map.addLayer(layer, false);
+    var sql = 'SELECT * FROM food_worker_orgs';
+    $.getJSON('http://fcwa.cartodb.com/api/v2/sql?format=GeoJSON&q=' + sql, function (data) {
+        L.geoJson(data, {
+            onEachFeature: function (feature, layer) {
+                layer.on('click', function () {
+                    map.fire('featureclick', feature.properties);
+                });
+            }
+        }).addTo(map);
     });
 
     return map;
