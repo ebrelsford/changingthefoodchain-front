b9f30186340be7567e168b5eaaa1141aa46c2074
Eric Brelsford <ebrelsford@gmail.com>
2014-08-13 15:53:49 -0400

    Fix missing header key

--- translations/es.js
+++ translations/es.js
@@ -29,6 +29,14 @@ helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
 
   return "Changing the Food Chain";
   }),
+        header: t(function (Handlebars,depth0,helpers,partials,data) {
+  this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
+  
+
+
+  return "Changing the<br />Food Chain";
+  }),
         nav: {
             about: t(function (Handlebars,depth0,helpers,partials,data) {
   this.compilerInfo = [4,'>= 1.0.0'];
