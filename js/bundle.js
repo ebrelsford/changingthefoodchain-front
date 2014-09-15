9ab3b1a780a766864c89451138825e701be8c529
Eric Brelsford <ebrelsford@gmail.com>
2014-09-15 12:31:05 -0400

    Keep link from competing with blur

--- js/bundle.js
+++ js/bundle.js
@@ -74574,11 +74574,13 @@ function program13(depth0,data) {
   data.buffer.push(" ");
   stack1 = helpers._triageMustache.call(depth0, "geocodedZip", {hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["ID"],data:data});
   if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
-  data.buffer.push("\n                    </div>\n                    <div class=\"add-organization-location-output-address-accept\">\n                        <a href=\"#\" ");
-  data.buffer.push(escapeExpression(helpers.action.call(depth0, "useGeocodedAddress", {hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["STRING"],data:data})));
-  data.buffer.push(">\n                            ");
+  data.buffer.push("\n                    </div>\n                    <div class=\"add-organization-location-output-address-accept\" ");
+  data.buffer.push(escapeExpression(helpers.action.call(depth0, "useGeocodedAddress", {hash:{
+    'on': ("mouseDown")
+  },hashTypes:{'on': "STRING"},hashContexts:{'on': depth0},contexts:[depth0],types:["STRING"],data:data})));
+  data.buffer.push(">\n                        ");
   data.buffer.push(escapeExpression((helper = helpers.t || (depth0 && depth0.t),options={hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["STRING"],data:data},helper ? helper.call(depth0, "organization_add.location.accept", options) : helperMissing.call(depth0, "t", "organization_add.location.accept", options))));
-  data.buffer.push("\n                        </a>\n                    </div>\n                </div>\n                ");
+  data.buffer.push("\n                    </div>\n                </div>\n                ");
   return buffer;
   }
 
